af8b2c9d4b0d624bd5c07c0238ee4c4800985647
Temi Taylor <temit@mit.edu>
2019-10-04 02:59:55 -0400

    Forcing the player to stay in bounds; needs review

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,16 +15,42 @@ public class PlayerMovement : MonoBehaviour
     private bool smoothInput = false;
 
     private Rigidbody rBody; //it complains if you try to name this rigidbody
+    private List<Bounds> thresholds;
+    private Vector3 lastValidPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         this.rBody = transform.GetComponent<Rigidbody>();
+
+        /* With the thresholds as triggers, it seems like the only ways to keep the player
+         * inside them is to either try to reset the position when OnTriggerExit is called
+         * (which has more ambiguous timing) or to do this; neither is particularly clean,
+         * and it may be better to consider just surrounding the play area with normal
+         * invisible colliders instead
+         */
+        this.thresholds = new List<Bounds>();
+        GameObject[] thresholdObjects = GameObject.FindGameObjectsWithTag("Threshold");
+        foreach(GameObject thresholdObject in thresholdObjects)
+        {
+            this.thresholds.Add(thresholdObject.GetComponent<BoxCollider>().bounds);
+        }
+        this.lastValidPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    // FixedUpdate is usually better for physics things
+    void FixedUpdate()
     {
+        foreach (Bounds thresholdBound in this.thresholds)
+        {
+            if (!thresholdBound.Contains(transform.position))
+            {
+                //return the player in-bounds if they escape
+                transform.position = lastValidPosition;
+            }
+        }
+        lastValidPosition = transform.position;
+
         Vector3 movement = this.getInputVector() * this.speed;
         this.rBody.velocity = movement;
     }
@@ -33,7 +59,7 @@ public class PlayerMovement : MonoBehaviour
      * Collects the player's movement inputs into a vector, accounting for whether or not
      * smooth input is requested. Assumes that movement is along the X-Z plane.
      **/
-    private Vector3 getInputVector()
+        private Vector3 getInputVector()
     {
         return smoothInput ?
             new Vector3(Input.GetAxis(HORI_INPUT), 0, Input.GetAxis(VERT_INPUT)):
