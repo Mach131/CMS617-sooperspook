b21bd206265859b26e15c5dfa1d84a41ab3ea70b
David Mueller <drmuelr@gmail.com>
2019-10-05 04:28:55 -0400

    add furniture placeholders + interaction script

---
+++ Assets/Scripts/DiegeticLabel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DiegeticLabel : MonoBehaviour
+{
+    public enum NameSource {Self, Other, Manual}
+    public NameSource nameSource = NameSource.Self;
+    
+    public TMPro.TextMeshPro[] textMeshes;
+    public GameObject otherSource;
+    public string manualString;
+
+    private void Awake() {
+        otherSource = gameObject;
+        manualString = otherSource.name;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string label = "";
+        
+        switch (nameSource) {
+        case NameSource.Manual:
+            label = manualString;
+            break;
+        case NameSource.Other:
+            label = otherSource.name;
+            break;
+        case NameSource.Self:
+            label = gameObject.name;
+            break;
+        default:
+            throw new System.Exception("Unhandled");
+        }
+
+        foreach( TMPro.TextMeshPro tmp in textMeshes ) {
+            tmp.text = label;
+        }
+
+    }
+}
