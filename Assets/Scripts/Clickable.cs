70b4d044660377394c7cac5e94df417c7f7c347f
Daniel Dangond <dadangond@gmail.com>
2019-11-07 12:48:33 -0500

    Added clickable object implementation and rough draft of trashcan and peel

---
+++ Assets/Scripts/Clickable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // MUST CALL base.Update()!!!
+    protected void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.GetComponent<Clickable>() != null && hit.transform.GetComponent<Clickable>().gameObject == gameObject ||
+                    hit.transform.GetComponentInParent<Clickable>() != null && hit.transform.GetComponentInParent<Clickable>().gameObject == gameObject)
+                {
+                    OnClick();
+                }
+            }
+        }
+    }
+
+    // Override this method to implement click events
+    protected virtual void OnClick()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Trashcan.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trashcan : Clickable
+{
+    public Animator orangePeel;
+
+    private bool isUpright = true;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    new protected void Update()
+    {
+        base.Update();
+    }
+
+    void Shake()
+    {
+        animator.SetTrigger("Shake");
+    }
+
+    void KnockOver()
+    {
+        animator.SetTrigger("KnockOver");
+        orangePeel.SetTrigger("FallOut");
+    }
+
+    protected override void OnClick()
+    {
+        if (isUpright)
+        {
+            KnockOver();
+            isUpright = false;
+        }
+        else
+        {
+            Shake();
+        }
+    }
+
+    public bool IsUpright()
+    {
+        return isUpright;
+    }
+}
