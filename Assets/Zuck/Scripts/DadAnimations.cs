44435e8a5bae379c8dd420b96b99ad016262b46d
jynnie <jynnie@mit.edu>
2019-11-15 16:47:19 -0500

    Dad prefab that looks at things

---
+++ Assets/Zuck/Scripts/DadAnimations.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DadAnimations : MonoBehaviour
+{
+    public Transform head;
+    public Transform testthis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LookAt(testthis));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }    
+
+    public IEnumerator LookAt(Transform thisthing)
+    {   
+        while(true)
+        {
+            if (thisthing == null) {
+                head.rotation = Quaternion.Euler(-10.6, 110.414, 21.2);
+            }
+            head.LookAt(thisthing);
+            yield return null;
+        }   
+    }
+}
