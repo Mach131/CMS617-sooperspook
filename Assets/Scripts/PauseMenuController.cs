9486cd42b8637e59971c0d71c1a528be8c570586
David Mueller <drmuelr@gmail.com>
2019-12-10 07:29:53 -0500

    make pause menu resume button work

--- Assets/Scripts/PauseMenuController.cs
+++ Assets/Scripts/PauseMenuController.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PauseMenuController : MonoBehaviour, IPausable
 {
     private static string gameSceneName = "MainScene";
-    private bool creditsPanelOpen;
 
-    public Button[] mainMenuButtons;
-    public GameObject creditsPanel;
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private GraphicRaycaster graphicRaycaster;
+
+    void Awake()
+    {
+        //canvas = GetComponent<Canvas>();
+        //graphicRaycaster = GetComponent<GraphicRaycaster>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        creditsPanelOpen = false;
-
 		// Register this object for pause/unpause updates
 		SceneDirector.GetPrimary().Register(this);
 	}
@@ -27,40 +31,8 @@ public class PauseMenuController : MonoBehaviour, IPausable
         SceneManager.LoadScene(gameSceneName);
     }
 
-    // Opens the credits panel, if it is not already open.
-    public void OnCreditsButtonPress()
-    {
-        if (!creditsPanelOpen)
-        {
-            // Disable main menu buttons so they can't be pressed anymore
-            foreach (Button button in mainMenuButtons)
-            {
-                button.interactable = false;
-            }
-            // Bring up the panel
-            creditsPanel.SetActive(true);
-
-            creditsPanelOpen = true;
-        }
-    }
-
-    public void OnCreditsReturnButtonPress()
-    {
-        if (creditsPanelOpen)
-        {
-            // Remove the panel
-            creditsPanel.SetActive(false);
-            // Re-enable main menu buttons
-            foreach (Button button in mainMenuButtons)
-            {
-                button.interactable = true;
-            }
-
-            creditsPanelOpen = false;
-        }
-    }
-
 	public void OnResumeButtonPress() {
+        Debug.Log("Resume");
 		SceneDirector.GetPrimary().Resume();
 	}
 
@@ -68,12 +40,27 @@ public class PauseMenuController : MonoBehaviour, IPausable
 		Application.Quit();
 	}
 
+    private void Hide()
+    {
+        canvas.enabled = false;
+        graphicRaycaster.enabled = false;
+        //canvas.gameObject.SetActive(false);
+        //graphicRaycaster.gameObject.SetActive(false);
+    }
+
+    private void Reveal()
+    {
+        canvas.enabled = true;
+        graphicRaycaster.enabled = true;
+        //canvas.gameObject.SetActive(true);
+        //graphicRaycaster.gameObject.SetActive(true);
+    }
 	
 	public void OnPause() {
-		
-	}
+        Reveal();
+    }
 
 	public void OnResume() {
-		
-	}
+        Hide();
+    }
 }
